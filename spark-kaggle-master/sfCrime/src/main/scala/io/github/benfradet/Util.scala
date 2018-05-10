3b71ba0cd218cfac392574a6edef6f240b2c187b
Vijay37 <reachpallaharsha@gmail.com>
2018-05-10 15:21:13 -0400

    Implemented pipeline method and collected 150 articles in each category

--- spark-kaggle-master/sfCrime/src/main/scala/io/github/benfradet/Util.scala
+++
@@ -1,9 +0,0 @@
-package io.github.benfradet
-
-import java.time.LocalDate
-
-object Util {
-  /** Compute the different dates between two dates */
-  def daysBetween(fromDate: LocalDate, toDate: LocalDate): Seq[LocalDate] =
-    fromDate.toEpochDay.to(toDate.toEpochDay).map(LocalDate.ofEpochDay)
-}
